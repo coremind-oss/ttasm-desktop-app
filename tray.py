7839a47e45caeb6cc88d100723318230064aa2a7
Dalibor Duric <dalibor@coremind.com.au>
2017-05-26 04:23:59 +1000

    playing with the exit function

--- tray.py
+++ tray.py
@@ -1,4 +1,8 @@
+import sys
+from time import sleep
+
 from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QMenu
 from PyQt5.QtWidgets import QSystemTrayIcon
 
 
@@ -6,3 +10,22 @@ class SystemTrayIcon(QSystemTrayIcon):
 
     def __init__(self):
         QSystemTrayIcon.__init__(self, QIcon('icons/icon-placeholder_128x128.png'))
+        
+        # Initialize menu
+        menu = QMenu()
+        exitButton = menu.addAction("Exit")
+        exitButton.triggered.connect(self.quit)
+                
+        # Set the menu as context menu
+        self.setContextMenu(menu)
+        
+    def quit(self):
+        for i in range(5):
+            self.showMessage(
+                'title {}'.format(i + 1),
+                'Quitting in {} seconds'.format(i + 1),
+                QSystemTrayIcon.Warning,
+                500,
+            )
+            sleep(1)
+        sys.exit(0)
