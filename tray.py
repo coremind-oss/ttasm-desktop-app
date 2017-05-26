fdbdb2fe65a1207c23974fc94a0dd0ed4a571eb2
Luka <lulo.djordjevic@gmail.com>
2017-05-26 11:09:51 +0200

    Added submenus example code

--- tray.py
+++ tray.py
@@ -12,20 +12,50 @@ class SystemTrayIcon(QSystemTrayIcon):
         QSystemTrayIcon.__init__(self, QIcon('icons/icon-placeholder_128x128.png'))
         
         # Initialize menu
-        menu = QMenu()
-        exitButton = menu.addAction("Exit")
+        mainMenu = QMenu()
+        mainMenu.setTitle("Main Menu")
+        
+        # Create submenus and populate them with items
+        subMenu1 = QMenu(mainMenu)
+        subMenu1.setTitle('Submenu1') # Not sure what this does
+        subMenu1ButtonA=subMenu1.addAction("Action A")
+        subMenu1ButtonA.triggered.connect(self.subMenu1ButtonAAction)
+        subMenu1.addAction("Action B")
+        subMenu1.addAction("Action B")
+
+        subMenu2 = QMenu(mainMenu)
+        subMenu2.setTitle('Submenu2')
+        subMenu2.addAction("Action D")
+        subMenu2.addAction("Action E")
+        subMenu2.addAction("Action F")      
+          
+        # Populate Main Menu
+        mainMenu.addMenu(subMenu1)
+        menu1Button = mainMenu.addAction("Menu1")
+        menu1Button.triggered.connect(self.menu1Action)
+       
+        mainMenu.addMenu(subMenu2)
+        
+        exitButton = mainMenu.addAction("Exit")
         exitButton.triggered.connect(self.quit)
-                
+ 
+ 
+                 
         # Set the menu as context menu
-        self.setContextMenu(menu)
+        self.setContextMenu(mainMenu)
+   
+    def menu1Action(self):
+        print ("MainMenu button2 stuff")
+        
+    def subMenu1ButtonAAction(self):
+        print ("Submenu1 First button stuff")
         
     def quit(self):
-        for i in range(5):
-            self.showMessage(
-                'title {}'.format(i + 1),
-                'Quitting in {} seconds'.format(i + 1),
-                QSystemTrayIcon.Warning,
-                500,
-            )
-            sleep(1)
+#         self.showMessage(
+#         'title {}'.format(1),
+#         'Quitting in {} seconds'.format(1),
+#         QSystemTrayIcon.Warning,
+#         500)
+#         sleep(1)
+        print ('Clean exit')
         sys.exit(0)
