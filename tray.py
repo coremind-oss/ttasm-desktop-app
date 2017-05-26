3831d604d248e2c54d5b8119c8a9de306824f0cc
Pekmen <milanpekovic89@gmail.com>
2017-05-26 18:20:02 +0200

    basic desktop app layout

--- tray.py
+++ tray.py
@@ -1,31 +1,76 @@
 import sys
-from time import sleep
 
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QMenu
 from PyQt5.QtWidgets import QSystemTrayIcon
 
+from login import LoginForm
+
 
 class SystemTrayIcon(QSystemTrayIcon):
 
+
     def __init__(self):
-        QSystemTrayIcon.__init__(self, QIcon('icons/icon-placeholder_128x128.png'))
-        
-        # Initialize menu
-        menu = QMenu()
-        exitButton = menu.addAction("Exit")
+        icon = QIcon('icons/icon-placeholder_128x128.png')
+        super(SystemTrayIcon, self).__init__(icon)
+
+        self.create_ui()
+
+
+    def create_ui(self):
+        """Create user interface of Tray icon"""
+
+        mainMenu = QMenu()
+
+        # Create sub menu with 2 actions
+        # When creating a sub menu, main menu must be passed as an argument to QMenu class
+        subMenu = QMenu(mainMenu)
+        subMenu.setTitle("Submenu")
+        subButton_1 = subMenu.addAction("Action 1")
+        subButton_1.triggered.connect(self.action_1)
+        subButton_2 = subMenu.addAction("Action 2")
+        subButton_2.triggered.connect(self.action_2)
+
+        # Set the order of layout and add everything to main menu
+        logInButton = mainMenu.addAction("Log in")
+        logInButton.triggered.connect(self.login)
+        mainMenu.addSeparator()
+        mainMenu.addMenu(subMenu)
+        mainMenu.addSeparator()
+        exitButton = mainMenu.addAction("Exit")
         exitButton.triggered.connect(self.quit)
-                
+
         # Set the menu as context menu
-        self.setContextMenu(menu)
-        
+        self.setContextMenu(mainMenu)
+
+
+    def login(self):
+        """Authenticate via webserver."""
+
+        # Keeping reference to LoginForm object so that window wouldn't close
+        self.loginForm = LoginForm()
+        self.loginForm.show()
+
+
+    def action_1(self):
+        """Placeholder function"""
+
+        self.showMessage('Action 1',
+                         'You triggered action 1',
+                         QSystemTrayIcon.Information,
+                         3000)
+
+
+    def action_2(self):
+        """Placeholder function"""
+
+        self.showMessage('Action 2',
+                         'You triggered action 2',
+                         QSystemTrayIcon.Information,
+                         3000)
+
+
     def quit(self):
-        for i in range(5):
-            self.showMessage(
-                'title {}'.format(i + 1),
-                'Quitting in {} seconds'.format(i + 1),
-                QSystemTrayIcon.Warning,
-                500,
-            )
-            sleep(1)
+        """Exit program in a clean way."""
+
         sys.exit(0)
