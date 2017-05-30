291df8cf24f29f2f2f8863a4327ff1822db7016e
Dalibor Duric <dalibor@coremind.com.au>
2017-05-31 04:18:51 +1000

    experimenting with shorty's code

--- client.py
+++ client.py
@@ -3,16 +3,14 @@ import socket
 client_socket = socket.socket()
 
 host = socket.gethostname()
+ip = socket.gethostbyname(host)
 
 port = 5000
 
-address=(host,port)
+address = (ip, port)
 
 client_socket.connect(address)
 
 print(client_socket.recv(1024))
 
 client_socket.close()
-
-
-
--- server.py
+++ server.py
@@ -1,24 +1,35 @@
+from _socket import SHUT_RDWR
 import socket
+
 
 server_socket = socket.socket()
 
 host = socket.gethostname()
+ip = socket.gethostbyname(host)
 
 port = 5000
 
-add = (host,port)
+address = (ip, port)
 
-server_socket.bind(add)
+server_socket.bind(address)
 
 server_socket.listen(1)
 
 
-
-while True:
-    conn, addr = server_socket.accept()
-    print('I have connection from this address', addr)
-    conn.send(b' you have been connected to our server')
-    
+try:
+    while True:
+        print('waiting for a client to connect on {}:{}'.format(ip, port))
+        conn, addr = server_socket.accept()
+        kwargs = {
+            'ip': addr[0],
+            'port': addr[1], 
+        }
+        print('---> I have connection on port {port} from this address {ip}:{port}'.format(**kwargs))
+        conn.send(b' you have been connected to our server')
+        
+        conn.close()
+except Exception as e:
+    print(e)
     conn.close()
-
+    server_socket.shutdown(SHUT_RDWR)
 
