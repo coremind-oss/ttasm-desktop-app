3a3febb25ecf3b058866a84e25cdcc6aad524275
Shortylo85 <daneshorty@gmail>
2017-05-31 15:09:17 +0200

    Some changes and inuput password for checking on server

--- client.py
+++ client.py
@@ -1,16 +1,43 @@
+# import socket
+# 
+# client_socket = socket.socket()
+# 
+# host = socket.gethostname()
+# ip = socket.gethostbyname(host)
+# 
+# port = 5000
+# 
+# address = (ip, port)
+# 
+# client_socket.connect(address)
+# 
+# print(client_socket.recv(1024))
+# 
+# client_socket.close()
+
 import socket
+from pip._vendor.distlib.compat import raw_input
+from builtins import bytes
 
-client_socket = socket.socket()
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = socket.gethostname()
-ip = socket.gethostbyname(host)
+ip = socket.gethostname()
 
 port = 5000
 
-address = (ip, port)
+address = (ip,port)
 
 client_socket.connect(address)
 
-print(client_socket.recv(1024))
+data = raw_input('#### Enter your password to pass server authentication and press enter:')
 
+client_socket.send(data.encode('utf_8'))
+  
+print('### Password are sent...')
+  
+print(client_socket.recv(2048))
+  
 client_socket.close()
+
+
+
--- server.py
+++ server.py
@@ -1,35 +1,71 @@
-from _socket import SHUT_RDWR
+# from _socket import SHUT_RDWR
+# import socket
+# 
+# 
+# server_socket = socket.socket()
+# 
+# host = socket.gethostname()
+# ip = socket.gethostbyname(host)
+# 
+# port = 5000
+# 
+# address = (ip, port)
+# 
+# server_socket.bind(address)
+# 
+# server_socket.listen(1)
+# 
+# 
+# try:
+#     while True:
+#         print('waiting for a client to connect on {}:{}'.format(ip, port))
+#         conn, addr = server_socket.accept()
+#         kwargs = {
+#             'ip': addr[0],
+#             'port': addr[1], 
+#         }
+#         print('---> I have connection on port {port} from this address {ip}:{port}'.format(**kwargs))
+#         conn.send(b' you have been connected to our server')
+#         
+#         conn.close()
+# except Exception as e:
+#     print(e)
+#     conn.close()
+#     server_socket.shutdown(SHUT_RDWR)
+
 import socket
 
-
-server_socket = socket.socket()
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
+
 ip = socket.gethostbyname(host)
 
 port = 5000
 
-address = (ip, port)
+address = (ip,port)
 
 server_socket.bind(address)
 
 server_socket.listen(1)
 
-
-try:
-    while True:
-        print('waiting for a client to connect on {}:{}'.format(ip, port))
-        conn, addr = server_socket.accept()
-        kwargs = {
-            'ip': addr[0],
-            'port': addr[1], 
-        }
-        print('---> I have connection on port {port} from this address {ip}:{port}'.format(**kwargs))
-        conn.send(b' you have been connected to our server')
+while True:
+    print('----> Waiting communication from {}{}'.format(ip,port))
+    client,addr = server_socket.accept()
+    print('Accepted communication from IP: ',addr[0], ' and port: ', addr[1])
+    data = client.recv(2048)
+    d=data.decode()
+    print('----> We recieved from client this data: ', d)
+    print('Processing the data...')
+    if d == "password":
+        client.send(b'Your password is correct!')
+        print('----> Password was correct')
+    else:
+        client.send(b'Incorrect password')
+        print('----> This password was incorrect')
+        client.close()
+    
         
-        conn.close()
-except Exception as e:
-    print(e)
-    conn.close()
-    server_socket.shutdown(SHUT_RDWR)
+    
+
 
