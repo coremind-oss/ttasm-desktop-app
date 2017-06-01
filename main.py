c15e25539a93e0bb5649800eb5816c065fa8b753
Luka <lulo.djordjevic@gmail.com>
2017-06-01 15:34:10 +0200

    Adeed prevention of running multiple instances

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-import sys
+import sys, os
 from PyQt5.QtWidgets import QApplication
 from tray import SystemTrayIcon
 
+def alreadyRunnig(name):
+    os.system('ps -ef > /tmp/ttasm')
+    lines=open('/tmp/ttasm', 'r').read().split('\n')
+    count=0
+    for line in lines:
+        if name in line:
+            count+=1
+    print ('Found {} mentions of ttasm in ps'.format(count))
+    if (count>1):
+        return True
+    else:
+        return False
 
 def main():
     app = QApplication([])
@@ -11,4 +23,11 @@ def main():
     systemTrayIcon.show()
     sys.exit(app.exec_())
 
-if __name__ == '__main__': main()
+if __name__ == '__main__': 
+    
+    pid = str(os.getpid())
+    print (pid)
+    if (alreadyRunnig("ttasm-desktop-app")):
+        print ('Ttasm already running, exiting...')
+    else:
+        main()
