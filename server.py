28a61abf31a0a586b864323b4af4876a42f02710
Shortylo85 <daneshorty@gmail>
2017-06-03 11:39:07 +0200

    Example of multi threaded port scanner

--- server.py
+++ server.py
@@ -85,65 +85,65 @@
     
 ################### Single threaded port scanner   
 
-import socket
-  
-soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  
-host = socket.gethostname()
-
-ip = socket.gethostbyname(host)
-  
-def port_scener(port):
-    try:
-        soc.connect((ip,port))
-        return True
-    except:
-        return False
-      
-for port in range(1,60000):
-    if port_scener(port):
-        print('Port ',port,' is opened')
+# import socket
+#   
+# soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#   
+# host = socket.gethostname()
+# 
+# ip = socket.gethostbyname(host)
+#   
+# def port_scener(port):
+#     try:
+#         soc.connect((ip,port))
+#         return True
+#     except:
+#         return False
+#       
+# for port in range(1,60000):
+#     if port_scener(port):
+#         print('Port ',port,' is opened')
     
 
 ############## Multithreaded Threaded port scenner with workers
 
-# import socket
-# import threading
-# from queue import Queue
-# from multiprocessing.pool import worker
-# 
-# print_lock = threading.Lock()
-# 
-# target = 'localhost'
-# 
-# def port_scanner(port):
-#     try:
-#         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         conn = sock.connect((target,port))
-#         with print_lock:
-#             print('Port: ',port,' is opened!!!')
-#         conn.close()
-#     except:
-#         pass
-#     
-#     
-# def threader():
-#     while True:
-#         worker = q.get()
-#         port_scanner(worker)
-#         q.task_done()
-# 
-# q = Queue()
-# 
-# for i in range(100):
-#     t = threading.Thread(target=threader)
-#     t.deamon = True
-#     t.start()
-#     
-# for worker in range(1,60000):
-#     q.put(worker)
-#     
-# q.join()
+import socket
+import threading
+from queue import Queue
+from multiprocessing.pool import worker
+ 
+print_lock = threading.Lock()
+ 
+target = 'localhost'
+ 
+def port_scanner(port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        conn = sock.connect((target,port))
+        with print_lock:
+            print('Port: ',port,' is opened!!!')
+        conn.close()
+    except:
+        pass
+     
+     
+def threader():
+    while True:
+        worker = q.get()
+        port_scanner(worker)
+        q.task_done()
+ 
+q = Queue()
+ 
+for i in range(100):
+    t = threading.Thread(target=threader)
+    t.deamon = True
+    t.start()
+     
+for worker in range(1,60000):
+    q.put(worker)
+     
+q.join()
     
  
         
