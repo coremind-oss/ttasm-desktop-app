aceacd6d3170cd7a4c8adb512ccc494fcf2bfdf3
Pekmen <milanpekovic89@gmail.com>
2017-06-08 18:22:52 +0200

    implemented dedicated connection functionality

--- client.py
+++ client.py
@@ -1,33 +1,52 @@
 import json
 import socket
+import threading
 
 
 SERVER_IP = '127.0.1.1'
-SERVER_ACCESS_PORT = 50000
+SERVER_ACCESS_PORT = 50001
 CHUNK_SIZE = 5
 
 
-class ClientThread():
+class Client():
     def __init__(self, server_ip, server_access_port):
         self.server_ip = server_ip
         self.server_access_port = server_access_port
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.get_dedicated_port()
+        self.initial_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.dedicated_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def run(self):
+        dedicated_port = int(self.get_dedicated_port())
+        self.dedicated_conection(self.dedicated_sock, self.server_ip, dedicated_port)
+
 
     def get_dedicated_port(self):
-        self.sock.connect((self.server_ip, self.server_access_port))
+        self.initial_sock.connect((self.server_ip, self.server_access_port))
         print("CONNECTED TO {}:{}".format(SERVER_IP, SERVER_ACCESS_PORT))
-        self.receive_message(self.sock)
+        new_port = self.receive_message(self.initial_sock)
+        print("NEW PORT IS {}".format(new_port))
+        self.initial_sock.close()
+        return (new_port)
 
     def receive_message(self, sock):
         data = []
         while (True):
-            data_chunk = self.sock.recv(CHUNK_SIZE)
+            data_chunk = sock.recv(CHUNK_SIZE)
             data_decoded = data_chunk.decode()
             data.append(data_decoded)
             if data_chunk[-1] == '\x00':
                 break
-        print(''.join(data))
+        data = data[:-1]
+        return (''.join(data))
+
+    def dedicated_conection(self, sock, server_ip, dedicated_port):
+        sock.connect((server_ip, dedicated_port))
+        print("[DEDICATED] CONNECTED TO {}:{}", server_ip, dedicated_port)
+
+
 
 if __name__ == '__main__':
-    clientThread = ClientThread(SERVER_IP, SERVER_ACCESS_PORT)
+    client = Client(SERVER_IP, SERVER_ACCESS_PORT)
+    clientThread = threading.Thread(target=client.run)
+    #clientThread.daemon = True
+    clientThread.start()
--- server.py
+++ server.py
@@ -1,14 +1,29 @@
 import json
 import socket
-from threading import Thread
+import threading
 from collections import deque
 
-ACCESS_PORT = 50000
+ACCESS_PORT = 50001
 SERVER_HOST = socket.gethostname()
 SERVER_IP = socket.gethostbyname(SERVER_HOST)
 CHUNK_SIZE = 1024
 
-available_ports = deque(range(50001, 60000))
+available_ports = deque(range(50001, 60001))
+
+
+class DedicatedClientConnection():
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def run(self):
+        with self.sock as s:
+            s.bind((self.ip, self.port))
+            s.listen(1)
+            while True:
+                conn, (cli_ip, cli_port) = s.accept()
+                print('[T] GOT CONNECTION FROM {}:{}'.format(cli_ip, cli_port))
 
 
 class SwitchWorker():
@@ -22,11 +37,16 @@ class SwitchWorker():
             s.bind((self.ip, self.port))
             s.listen(5)
             while True:
-                    conn, addr = s.accept()
-                    print('CONNECTION VARIABLE', conn)
-                    dedicated_port = available_ports.pop()
-                    server_response = str(dedicated_port) + "\x00"
-                    conn.send(server_response.encode())
+                conn, (cli_ip, cli_port) = s.accept()
+                print('[SW] GOT CONNECTION FROM {}:{}'.format(cli_ip, cli_port))
+                dedicated_port = available_ports.pop()
+                server_response = "{}{}".format(str(dedicated_port), "\x00")
+                conn.send(server_response.encode())
+
+                dedicatedConnection = DedicatedClientConnection(self.ip, dedicated_port)
+                dedicatedConnectionThread = threading.Thread(target=dedicatedConnection.run)
+                dedicatedConnectionThread.start()
+
 
 
 
