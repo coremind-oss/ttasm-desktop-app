9a02d12449ebc824977509963ae120b8d3f690fb
Pekmen <milanpekovic89@gmail.com>
2017-06-09 13:40:35 +0200

    cleaned up a bit

--- client.py
+++ client.py
@@ -5,11 +5,15 @@ import sys
 
 
 SERVER_IP = '127.0.1.1'
-SERVER_ACCESS_PORT = 50009
+SERVER_ACCESS_PORT = 50000
 CHUNK_SIZE = 2
 
 
 class Client():
+    """ Class that handles all client side communication.
+        It initialy connects to server just to get this client's dedicated port number,
+        then discards initial socket. Communication is continued on dedicated port with new socket """
+
     def __init__(self, server_ip, server_access_port):
         self.server_ip = server_ip
         self.server_access_port = server_access_port
@@ -19,28 +23,34 @@ class Client():
 
     def run(self):
         dedicated_port = int(self.get_dedicated_port())
+
+        self.initial_sock.close()
+        print("Connection with {} closed".format(self.server_access_port))
+
         self.dedicated_conection(self.dedicated_sock, self.server_ip, dedicated_port)
 
 
     def get_dedicated_port(self):
+        """ Connect to server, receive dedicated port number and return it """
         self.initial_sock.connect((self.server_ip, self.server_access_port))
         print("Conected to {}:{}".format(SERVER_IP, SERVER_ACCESS_PORT))
 
         new_port = self.receive_message(self.initial_sock)
         print("Dedicated port is {}".format(new_port))
 
-        self.initial_sock.close()
-        print("Connection with {} closed".format(self.server_access_port))
-
         return (new_port)
 
 
     def receive_message(self, sock):
+        """ Collect arriving message on given socket, decode it and remove null char from end """
+
         data = []
         while (True):
             data_chunk = sock.recv(CHUNK_SIZE)
             data_decoded = data_chunk.decode()
             data.append(data_decoded)
+
+            # null char is used to signal the end of message
             if data_decoded[-1] == '\x00':
                 break
 
@@ -49,32 +59,36 @@ class Client():
 
 
     def send_message(self, sock, message):
+        """ Send decoded message through given socket with appended null char """
+
+        # null char is used to signal the end of message
         message = message + '\x00'
         sock.send(message.encode())
 
 
     def dedicated_conection(self, sock, server_ip, dedicated_port):
-        print('[DEDICATED] trying connection on {}:{}'.format(server_ip, dedicated_port))
+        """ Create new connection with dedicated port, and send user input to server """
+
+        print('Trying connection on dedicated {}:{}'.format(server_ip, dedicated_port))
         try:
             sock.connect((server_ip, dedicated_port))
         except:
-            print('[DEDICATED] unexpected error, shutting down')
+            print('Unexpected connection error, shutting down')
             sock.close()
             sys.exit()
 
-        print("[DEDICATED] connected to {}:{}".format(server_ip, dedicated_port))
+        print("Connected to {}:{}".format(server_ip, dedicated_port))
+
+        # Send user input to server, and read whatever server sends back. FOREVER!
         while True:
             test_message = raw_input('[DEDICATED PORT {}] enter your message: '.format(dedicated_port))
             self.send_message(sock, test_message)
-            print('[DEDICATED PORT {}] sent message - {}'.format(dedicated_port, test_message))
             msg = self.receive_message(sock)
-            print(msg)
+            print('[SERVER MESSAGE] here is your message reversed - <{}>'.format(msg))
 
 
 
 if __name__ == '__main__':
     client = Client(SERVER_IP, SERVER_ACCESS_PORT)
-    client.run()
     clientThread = threading.Thread(target=client.run)
-    clientThread.daemon = True
     clientThread.start()
--- server.py
+++ server.py
@@ -3,39 +3,58 @@ import socket
 import threading
 from collections import deque
 
-ACCESS_PORT = 50009
+ACCESS_PORT = 50000
 SERVER_HOST = socket.gethostname()
 SERVER_IP = socket.gethostbyname(SERVER_HOST)
 CHUNK_SIZE = 2
 
-available_ports = deque(range(50001, 56666))
+# THIS IS SOLUTION FOR TESTING ONLY
+# make a deque of 10 000 numbers to serve as our available ports pool
+# get ports by calling available_ports.pop()
+available_ports = deque(range(50001, 60000))
+
 
 
 class DedicatedClientConnection():
+    """ Class for handling dedicated connection with client, separated from main logic
+        Should always be called with Thread wrapper """
+
+
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
     def run(self):
         with self.sock as s:
             s.bind((self.ip, self.port))
             s.listen(1)
             print('[THREAD PORT {}] listening'.format(self.port))
+
             conn, (cli_ip, cli_port) = s.accept()
             print('[THREAD PORT {}] got connection from {}:{}'.format(self.port, cli_ip, cli_port))
+
+            # Expect message from client, then send reversed message back. FOREVER!
             while True:
                 client_msg = self.receive_message(conn)
-                print('[THREAD PORT {}] got message - {}'.format(self.port, client_msg))
-                self.send_message(conn, '[SERVER MESSAGE] here is your message reversed - ' + client_msg[::-1])
+                print('[THREAD PORT {}] got message - <{}>'.format(self.port, client_msg))
+
+                reversed_msg = client_msg[::-1] # reverse message
+                self.send_message(conn, reversed_msg)
+                print('[THREAD PORT {}] sent back reversed message - <{}>'.format(self.port, reversed_msg))
 
 
     def receive_message(self, connection):
+        """ Collect arriving message on connection, decode it and remove null char from end """
+
         data = []
         while (True):
             data_chunk = connection.recv(CHUNK_SIZE)
             data_decoded = data_chunk.decode()
             data.append(data_decoded)
+
+            # null char is used to signal the end of message
             if data_decoded[-1] == '\x00':
                 break
 
@@ -44,11 +63,20 @@ class DedicatedClientConnection():
 
 
     def send_message(self, connection, message):
+        """ Send decoded message through given connection with appended null char """
+
+        # null char is used to signal the end of message
         message = message + '\x00'
         connection.send(message.encode())
 
 
+
 class SwitchWorker():
+    """ Switch worker will just listen form initial connections from client.
+        On successfull conection it will send available dedicated port to client,
+        create a dedicated socket with dedicated port on separate thread (using DedicatedClientConnection class)
+        and move on to next client. All comunication will then be held on dedicated thread. """
+
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
@@ -64,15 +92,20 @@ class SwitchWorker():
             while True:
                 conn, (cli_ip, cli_port) = s.accept()
                 print('\n[SW] got connection from {}:{}'.format(cli_ip, cli_port))
+
                 dedicated_port = available_ports.pop()
                 server_response = "{}{}".format(str(dedicated_port), '\x00')
+
+                # Send encoded dedicated port number to client
                 print('[SW] sending port num {} to {}:{}'.format(server_response, cli_ip, cli_port))
                 conn.send(server_response.encode())
 
+                # Create dedicated separate thread for comunication with client
                 print('[SW] preparing connection for port {}'.format(dedicated_port))
                 dedicatedConnection = DedicatedClientConnection(self.ip, dedicated_port)
                 dedicatedConnectionThread = threading.Thread(target=dedicatedConnection.run)
                 dedicatedConnectionThread.start()
+
 
 
 if __name__ == "__main__":
