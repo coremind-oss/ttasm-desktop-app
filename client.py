19c88dff3933d5e4c6b5cbfa6911bf0048228af7
Pekmen <milanpekovic89@gmail.com>
2017-06-09 03:02:50 +0200

    two way communication

--- client.py
+++ client.py
@@ -1,11 +1,12 @@
 import json
 import socket
 import threading
+import sys
 
 
 SERVER_IP = '127.0.1.1'
-SERVER_ACCESS_PORT = 50001
-CHUNK_SIZE = 5
+SERVER_ACCESS_PORT = 50009
+CHUNK_SIZE = 2
 
 
 class Client():
@@ -15,6 +16,7 @@ class Client():
         self.initial_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.dedicated_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
     def run(self):
         dedicated_port = int(self.get_dedicated_port())
         self.dedicated_conection(self.dedicated_sock, self.server_ip, dedicated_port)
@@ -22,11 +24,16 @@ class Client():
 
     def get_dedicated_port(self):
         self.initial_sock.connect((self.server_ip, self.server_access_port))
-        print("CONNECTED TO {}:{}".format(SERVER_IP, SERVER_ACCESS_PORT))
+        print("Conected to {}:{}".format(SERVER_IP, SERVER_ACCESS_PORT))
+
         new_port = self.receive_message(self.initial_sock)
-        print("NEW PORT IS {}".format(new_port))
+        print("Dedicated port is {}".format(new_port))
+
         self.initial_sock.close()
+        print("Connection with {} closed".format(self.server_access_port))
+
         return (new_port)
+
 
     def receive_message(self, sock):
         data = []
@@ -34,19 +41,40 @@ class Client():
             data_chunk = sock.recv(CHUNK_SIZE)
             data_decoded = data_chunk.decode()
             data.append(data_decoded)
-            if data_chunk[-1] == '\x00':
+            if data_decoded[-1] == '\x00':
                 break
-        data = data[:-1]
-        return (''.join(data))
+
+        total_data = ''.join(data)[:-1]
+        return (total_data)
+
+
+    def send_message(self, sock, message):
+        message = message + '\x00'
+        sock.send(message.encode())
+
 
     def dedicated_conection(self, sock, server_ip, dedicated_port):
-        sock.connect((server_ip, dedicated_port))
-        print("[DEDICATED] CONNECTED TO {}:{}", server_ip, dedicated_port)
+        print('[DEDICATED] trying connection on {}:{}'.format(server_ip, dedicated_port))
+        try:
+            sock.connect((server_ip, dedicated_port))
+        except:
+            print('[DEDICATED] unexpected error, shutting down')
+            sock.close()
+            sys.exit()
+
+        print("[DEDICATED] connected to {}:{}".format(server_ip, dedicated_port))
+        while True:
+            test_message = raw_input('[DEDICATED PORT {}] enter your message: '.format(dedicated_port))
+            self.send_message(sock, test_message)
+            print('[DEDICATED PORT {}] sent message - {}'.format(dedicated_port, test_message))
+            msg = self.receive_message(sock)
+            print(msg)
 
 
 
 if __name__ == '__main__':
     client = Client(SERVER_IP, SERVER_ACCESS_PORT)
+    client.run()
     clientThread = threading.Thread(target=client.run)
-    #clientThread.daemon = True
+    clientThread.daemon = True
     clientThread.start()
--- server.py
+++ server.py
@@ -3,12 +3,12 @@ import socket
 import threading
 from collections import deque
 
-ACCESS_PORT = 50001
+ACCESS_PORT = 50009
 SERVER_HOST = socket.gethostname()
 SERVER_IP = socket.gethostbyname(SERVER_HOST)
-CHUNK_SIZE = 1024
+CHUNK_SIZE = 2
 
-available_ports = deque(range(50001, 60001))
+available_ports = deque(range(50001, 56666))
 
 
 class DedicatedClientConnection():
@@ -21,9 +21,31 @@ class DedicatedClientConnection():
         with self.sock as s:
             s.bind((self.ip, self.port))
             s.listen(1)
+            print('[THREAD PORT {}] listening'.format(self.port))
+            conn, (cli_ip, cli_port) = s.accept()
+            print('[THREAD PORT {}] got connection from {}:{}'.format(self.port, cli_ip, cli_port))
             while True:
-                conn, (cli_ip, cli_port) = s.accept()
-                print('[T] GOT CONNECTION FROM {}:{}'.format(cli_ip, cli_port))
+                client_msg = self.receive_message(conn)
+                print('[THREAD PORT {}] got message - {}'.format(self.port, client_msg))
+                self.send_message(conn, '[SERVER MESSAGE] here is your message reversed - ' + client_msg[::-1])
+
+
+    def receive_message(self, connection):
+        data = []
+        while (True):
+            data_chunk = connection.recv(CHUNK_SIZE)
+            data_decoded = data_chunk.decode()
+            data.append(data_decoded)
+            if data_decoded[-1] == '\x00':
+                break
+
+        total_data = ''.join(data)[:-1]
+        return (total_data)
+
+
+    def send_message(self, connection, message):
+        message = message + '\x00'
+        connection.send(message.encode())
 
 
 class SwitchWorker():
@@ -31,23 +53,26 @@ class SwitchWorker():
         self.ip = ip
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Initializing switch worker")
+
 
     def run(self):
+        print("Switch worker running")
         with self.sock as s:
             s.bind((self.ip, self.port))
             s.listen(5)
             while True:
                 conn, (cli_ip, cli_port) = s.accept()
-                print('[SW] GOT CONNECTION FROM {}:{}'.format(cli_ip, cli_port))
+                print('\n[SW] got connection from {}:{}'.format(cli_ip, cli_port))
                 dedicated_port = available_ports.pop()
-                server_response = "{}{}".format(str(dedicated_port), "\x00")
+                server_response = "{}{}".format(str(dedicated_port), '\x00')
+                print('[SW] sending port num {} to {}:{}'.format(server_response, cli_ip, cli_port))
                 conn.send(server_response.encode())
 
+                print('[SW] preparing connection for port {}'.format(dedicated_port))
                 dedicatedConnection = DedicatedClientConnection(self.ip, dedicated_port)
                 dedicatedConnectionThread = threading.Thread(target=dedicatedConnection.run)
                 dedicatedConnectionThread.start()
-
-
 
 
 if __name__ == "__main__":
