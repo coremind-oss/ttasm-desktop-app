8c3119e494dd6e69aa87e88a8e03af3758294a2b
Shortylo85 <daneshorty@gmail>
2017-07-03 21:20:15 +0200

    send_timestamp function now in stage 1 - deny empty fields and simply print message text

--- timestamp_form.py
+++ timestamp_form.py
@@ -29,26 +29,26 @@ class TimestampForm(QWidget):
         self.setWindowTitle('Message')
         
         self.create_ui()
-        self.desktop_timezone() 
+        
         
     def create_ui(self):
         
-        msgLabel = QLabel('What are you working on?')
+        msgLabel = QLabel('What were you doing in the period?')
         self.message = QLineEdit()
         
         self.message.setPlaceholderText('Enter text here...')
         
-#         sendButton = QPushButton('Send')
+        sendButton = QPushButton('Send')
         cancelButton = QPushButton('Cancel')
         
         cancelButton.clicked.connect(self.cancel)
-#         sendButton.clicked.connect(self.send_timestamp)
+        sendButton.clicked.connect(lambda: self.send_timestamp(self.message.text()))
         formBox = QFormLayout()
         formBox.addRow(msgLabel)
         formBox.addRow(self.message)
         
         buttonRow = QHBoxLayout()
-#         buttonRow.addWidget(sendButton)
+        buttonRow.addWidget(sendButton)
         buttonRow.addWidget(cancelButton)
         
         formBox.addRow(buttonRow)
@@ -58,7 +58,33 @@ class TimestampForm(QWidget):
         self.setFixedSize(self.height, self.width)
         
 #         just for checking
-        
+
+    def send_timestamp(self, message):
+
+        if not message:
+     
+            msgBox = QMessageBox()
+            text_info = [] 
+     
+            if not message:
+     
+                text_info.append('You have not send any message')
+                error_info_text = ''.join(text_info)
+                  
+                msgBox.setInformativeText(error_info_text)
+                msgBox.setIcon(QMessageBox.Information)
+                msgBox.setWindowTitle('Error')
+                msgBox.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+                msgBox.addButton(QMessageBox.Ok)
+                msgBox.exec()
+     
+        else:
+            self.desktop_timezone() 
+            print('\nSent message is{}'.format(message)) 
+
+#             url = '{}://{}/timestamp_message_handling/{}'.format('http','127.0.0.1:8000',message)
+            
+#             print(url)
         
 #     Finding a desktop IP and from that find a Desktop's timezone   
     def desktop_timezone(self):
@@ -88,7 +114,7 @@ class TimestampForm(QWidget):
         loc_dt = desktop_loc_time.localize(datetime.now())
         print ("\nLocal time for desktop location is: {}".format(loc_dt.strftime(fmt)))
 
-    
+
         
          
     
