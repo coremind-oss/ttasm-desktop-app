68eb914f74bc7a48a289835444b79fae314ccd36
Shortylo85 <daneshorty@gmail>
2017-07-04 02:28:35 +0200

    send_timestamp function now in stage 2 - send messages and time to server and server recognize it

--- timestamp_form.py
+++ timestamp_form.py
@@ -31,6 +31,7 @@ class TimestampForm(QWidget):
         self.create_ui()
         
         
+        
     def create_ui(self):
         
         msgLabel = QLabel('What were you doing in the period?')
@@ -57,35 +58,6 @@ class TimestampForm(QWidget):
         
         self.setFixedSize(self.height, self.width)
         
-#         just for checking
-
-    def send_timestamp(self, message):
-
-        if not message:
-     
-            msgBox = QMessageBox()
-            text_info = [] 
-     
-            if not message:
-     
-                text_info.append('You have not send any message')
-                error_info_text = ''.join(text_info)
-                  
-                msgBox.setInformativeText(error_info_text)
-                msgBox.setIcon(QMessageBox.Information)
-                msgBox.setWindowTitle('Error')
-                msgBox.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
-                msgBox.addButton(QMessageBox.Ok)
-                msgBox.exec()
-     
-        else:
-            self.desktop_timezone() 
-            print('\nSent message is{}'.format(message)) 
-
-#             url = '{}://{}/timestamp_message_handling/{}'.format('http','127.0.0.1:8000',message)
-            
-#             print(url)
-        
 #     Finding a desktop IP and from that find a Desktop's timezone   
     def desktop_timezone(self):
      
@@ -111,13 +83,75 @@ class TimestampForm(QWidget):
         desktop_loc_time = pytz.timezone(current_timezone)
         fmt = '%Y-%m-%d %H:%M:%S %Z%z'
         
-        loc_dt = desktop_loc_time.localize(datetime.now())
-        print ("\nLocal time for desktop location is: {}".format(loc_dt.strftime(fmt)))
+        self.loc_dt = desktop_loc_time.localize(datetime.now())
+        
+        self.formated_time = self.loc_dt.strftime(fmt)
+        
+        return self.formated_time
+        
+#         print ("\nLocal time for desktop location is: {}".format(self.formated_time))
+
+        
+
+    def send_timestamp(self, message):
+
+        if not message:
+     
+            msgBox = QMessageBox()
+            text_info = [] 
+     
+            if not message:
+     
+                text_info.append('You have not send any message')
+                error_info_text = ''.join(text_info)
+                  
+                msgBox.setInformativeText(error_info_text)
+                msgBox.setIcon(QMessageBox.Information)
+                msgBox.setWindowTitle('Error')
+                msgBox.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+                msgBox.addButton(QMessageBox.Ok)
+                msgBox.exec()
+     
+        else:
+            self.desktop_timezone()
+            
+            print('\nThis is the local time of the user: {} '.format(self.formated_time))
+
+            print('\nSent message is: {}'.format(message))
+
+            
+
+            url = '{}://{}/timestamp_message_handling/'.format('HTTP','127.0.0.1:8000')
+             
+            print("Trying to send data to {}".format(url))
+            
+            try:
+                response = requests.post(url, {'message': message, 'timestamp': self.formated_time})
+                
+            
+            except Exception as e:
+                print('there is no proper server')
+            print(response.text)   
+#             print('THIS IS RESPONSE\n\n{}'.format(response.text))
+
+            
+#             if response.text == 'Successfully sent message':
+#                 msgBox = QMessageBox()
+#                 msgBox.setInformativeText('You sent a message')
+#                 msgBox.setIcon(QMessageBox.Information)
+#                 msgBox.setWindowTitle("Message")
+#     
+#                 msgBox.setStandardButtons(QMessageBox.Ok)
+#                 msgBox.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+#                 msgBox.exec()
+#                 self.close() 
+            
+        
 
 
         
-         
-    
         
+
+  
     def cancel(self):
         self.hide()
